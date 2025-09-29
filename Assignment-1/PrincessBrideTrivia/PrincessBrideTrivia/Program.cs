861a3e0076ffd86dcf015607851044384437db9a
BillMillerCoding <billmillercoding@outlook.com>
2025-09-28 19:29:38 -0700

    w-miller: This is an addition of a new hardmode feature where the user must type out the answers instead of selecting them from a multiple choice sheet

--- Assignment-1/PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
+++ Assignment-1/PrincessBrideTrivia/PrincessBrideTrivia/Program.cs
@@ -4,13 +4,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        Console.Write("Princess Bride Trivia!!! Do you want to try hard mode?(y/n): ");
+        string choice = Console.ReadLine();
+        bool isHardMode = false;
+        if (String.Equals(choice, "y", StringComparison.OrdinalIgnoreCase))
+        {
+            isHardMode = true;
+            Console.WriteLine("HARD MODE ACTIVATED!!!!!!!!!!!!");
+        }
+        
         string filePath = GetFilePath();
         Question[] questions = LoadQuestions(filePath);
 
         int numberCorrect = 0;
         for (int i = 0; i < questions.Length; i++)
         {
-            bool result = AskQuestion(questions[i]);
+            
+            bool result = AskQuestion(questions[i], isHardMode);
+            
             if (result)
             {
                 numberCorrect++;
@@ -24,11 +35,16 @@ public class Program
         return (100 * numberCorrectAnswers / numberOfQuestions) + "%";
     }
 
-    public static bool AskQuestion(Question question)
+    //UPDATED THIS FUNCTION WITH PARAMETER isHard
+    public static bool AskQuestion(Question question, bool isHard)
     {
-        DisplayQuestion(question);
+        DisplayQuestion(question, isHard);
 
         string userGuess = GetGuessFromUser();
+        if (isHard)
+        {
+            return DisplayHardResult(userGuess, question);
+        }
         return DisplayResult(userGuess, question);
     }
 
@@ -37,6 +53,7 @@ public class Program
         return Console.ReadLine();
     }
 
+    //WE CANT CHANGE THIS METHOD BECAUSE IT IS RUNNING TESTS
     public static bool DisplayResult(string userGuess, Question question)
     {
         if (userGuess == question.CorrectAnswerIndex)
@@ -48,13 +65,28 @@ public class Program
         Console.WriteLine("Incorrect");
         return false;
     }
+    public static bool DisplayHardResult(string userGuess, Question question)
+    {
+        if (String.Equals(userGuess, question.Answers[int.Parse(question.CorrectAnswerIndex) - 1], StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("Correct");
+            return true;
+        }
 
-    public static void DisplayQuestion(Question question)
+        Console.WriteLine("Incorrect");
+        return false;
+    }
+
+    //UPDATED THIS FUNCTION WITH PARAMETER isHard
+    public static void DisplayQuestion(Question question, bool isHard)
     {
         Console.WriteLine("Question: " + question.Text);
-        for (int i = 0; i < question.Answers.Length; i++)
+        if (!isHard)
         {
-            Console.WriteLine((i + 1) + ": " + question.Answers[i]);
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ": " + question.Answers[i]);
+            }
         }
     }
 
