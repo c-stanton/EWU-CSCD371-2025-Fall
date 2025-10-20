fe627ff7c69832c87d8bbec235eac13f0f91ff29
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-19 17:24:06 -0700

    close namespace in fullname

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
-{
-    public record class FullName(string FirstName, string LastName, string MiddleName);
-}
+namespace Logger;
+
+public record class FullName(string FirstName, string LastName, string MiddleName);
