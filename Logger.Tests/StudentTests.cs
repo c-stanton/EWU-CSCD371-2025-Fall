819b575cf221230850b5a7b30fa3a0bcd6fec467
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-20 17:45:50 -0700

    Student Tests

--- Logger.Tests/StudentTests.cs
+++ Logger.Tests/StudentTests.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace Logger.Tests;
 
-internal class StudentTests
+public class StudentTests
 {
+    [Fact]
+    public void Student_GraduationYearProperty_IsSetCorrectly()
+    {
+        // Arrange
+        var fullName = new FullName("Alice", "B", "Johnson");
+        var graduationYear = "2025";
+        var student = new entities.Student(fullName, graduationYear);
+        // Act & Assert
+        Assert.Equal(graduationYear, student.GraduationYear);
+    }
+
+    [Fact]
+    public void Student_NameProperty_ReturnsCorrectFullName()
+    {
+        // Arrange
+        var fullName = new FullName("Bob", "C", "Williams");
+        var graduationYear = "2024";
+        var student = new entities.Student(fullName, graduationYear);
+        // Act
+        var name = student.Name;
+        // Assert
+        Assert.Equal("Bob C Williams", name);
+    }
+
+
 }
