23d53728cdb79053fb2bd3a14b22fccde055392d
Chloe Stanton <cstanton5@ewu.edu>
2025-10-31 13:59:02 -0700

    Created project structure

---
+++ Calculate/Calculate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculate;
+
+public class Program
+{
+    public required Action<string> WriteLine { get; init; }
+
+    public required Func<string?> ReadLine { get; init; }
+
+    public Program()
+    {
+
+    }
+
+    public static void Main(string[] args)
+    {
+        var program = new Program
+        {
+            WriteLine = Console.WriteLine,
+            ReadLine = Console.ReadLine
+        };
+        program.Run();
+    }
+
+    
+    public void Run()
+    {
+       
+    }
+
+}
---
+++ Calculate/CalculateTests.Tests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculate;
+
+namespace CalculateTests.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
